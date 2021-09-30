bcc9fe67cfa8704fe06d7e521366aed866995eaa
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-09-30 22:28:26 +0000

    added a comment to calculate median

--- Assignment1/helloworld.c
+++ Assignment1/helloworld.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(){
-int a = 3,b = 5,c = 6,d = 7,e = 13;
+int main() 
+{
+int a = 3,b = 5,c = 22,d = 7,e = 13;
 float w = (a + b + c + d + e)/5.0; 
 	printf("%f\n",w);
 }
+/*
+void Array_sort(int *array, int n)
+{
+int i=0, j=0, temp=0;
+	for (i=0; i<n; i++)
+	{
+		 for(j=0; j<n-1; j++)
+		 {	
+			if(array[j]>array[j+1]) 
+}
+}*/
