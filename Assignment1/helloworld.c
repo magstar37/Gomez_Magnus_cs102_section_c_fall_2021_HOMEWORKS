e5028eefff784470abe483105479664309c8f556
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-01 17:56:09 +0000

    got mode and created makefile

--- Assignment1/helloworld.c
+++ Assignment1/helloworld.c
@@ -2,59 +2,57 @@
 
 
 void swap(int *p,int *q) {
-	  int t;
-	  t=*p;
-     	  *p=*q;
-	  *q=t;
+	int t;
+	t=*p;
+	*p=*q;
+	*q=t;
 }
 
 void sort(int r[],int n) {
-	  int i,j,temp;
-		 for(i = 0;i < n-1;i++) {
-		 for(j = 0;j < n-i-1;j++) {			             
-		 if(r[j] > r[j+1])
-			 swap(&r[j],&r[j+1]);
-						        }
-			       }
+	int i,j,temp;
+	for(i = 0;i < n-1;i++) {
+		for(j = 0;j < n-i-1;j++) {			             
+			if(r[j] > r[j+1])
+				swap(&r[j],&r[j+1]);
+		}
+	}
 }
-int mode(int m[],int f) {
-int maxValue = 0, maxCount = 0, z, y;
-	for (z = 0; z < f; ++z) {
-	 int count = 0;
-			          
-	  for (y = 0; y < f; ++y) {
-		 if (m[y] == m[z])
-	           ++count;
-						         }
-				        
-	 if (count > maxCount) {
-	        maxCount = count;
-	        maxValue = m[z];
-								        }
-					   }
+int mode(int m[],int length) {
+	int maxValue = 0, maxCount = 0, i, j;
+	for (i = 0; i < length; ++i) {
+		int count = 0;
 
-	         return maxValue;
+		for (j = 0; j < length; ++j) {
+			if (m[j] == m[i])
+				++count;
+		}
+
+		if (count > maxCount) {
+			maxCount = count;
+			maxValue = m[i];
+		}
+	}
+
+	return maxValue;
 }
 
 
 
 int main() 
 {
-int a = 3,b = 5,c = 22,d = 7,e = 13;
-float w = (a + b + c + d + e)/5.0; 
+	int a = 3,b = 5,c = 22,d = 5,e = 13;
+	float w = (a + b + c + d + e)/5.0; 
 	printf("Mean = %f\n",w);
 
-	   int r[] = {a,b,c,d,e};
-	   int n = 5;
-	   int sum,i;
-		sort(r,n);
-		 n = (n+1) / 2 - 1;      
-	          printf("Median = %d\n ", r[n]);
-		     return 0;
-	   int f = 5;
-	      int m[] = {a,b,c,d,e};
-  	 printf("Mode = %d\n ", mode(m,f));    
+	int r[] = {a,b,c,d,e};
+	int n = 5;
+	int sum,i;
+	sort(r,n);
+
+	printf("Median = %d\n ", r[(n+1)/2 - 1]);
+	printf("Mode = %d\n ", mode(r,n));    
+
+	return 0;
 
 
-		     
 }
