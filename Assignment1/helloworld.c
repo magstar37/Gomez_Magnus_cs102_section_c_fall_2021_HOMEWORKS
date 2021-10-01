3cbe1a06e7a4f9dce4526364878126ca63d13958
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-01 03:23:48 +0000

    tried to do more of the median

--- Assignment1/helloworld.c
+++ Assignment1/helloworld.c
@@ -6,7 +6,7 @@ int a = 3,b = 5,c = 22,d = 7,e = 13;
 float w = (a + b + c + d + e)/5.0; 
 	printf("%f\n",w);
 }
-/*
+
 void Array_sort(int *array, int n)
 {
 int i=0, j=0, temp=0;
@@ -14,6 +14,18 @@ int i=0, j=0, temp=0;
 	{
 		 for(j=0; j<n-1; j++)
 		 {	
-			if(array[j]>array[j+1]) 
+			if(array[j]>array[j+1])
+			{
+			temp        =array[j];
+			array[j]    =array[j+1];
+			array[j+1]  =temp; }
 }
-}*/
+}
+for(i=0; i<n; i++)
+{	printf("\narray_1[%d] : %d",i,array[i]);
+}}
+float Find_median(int array[], int n)
+{
+	float median=0;
+	if (n%2 == 0)
+		median = 
