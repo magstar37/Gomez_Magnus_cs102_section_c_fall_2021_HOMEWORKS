cb46f6d78e7c6cf802a2e6e973e5ece64d4fb0a3
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-07 21:54:25 +0000

    started comparing strings

---
+++ Assignment1/string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+int main(void) {
+	char line[MAXLINE];
+	printf("Please enter your text:");
+	fgets(line, sizeof(line), stdin);
+
+	char line1[MAXLINE];
+	printf("Please enter your text:");
+	fgets(line1, sizeof(line1), stdin);
+	
+	
+	char line2[MAXLINE];
+	printf("Please enter your text:");
+	fgets(line2, sizeof(line2), stdin);
+
+ 
+		if (strcmp(line,line1)<0);{ /*checks if line is less that line1*/
+			strcmp(line<line2);
+
+				}
+
+
+	/*printf("You typed %s\n", line);
+	float f;
+	scanf(line, "%f", &f);*/
+
